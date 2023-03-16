849a49ddd42cd62a610cd226b549283f5a117217
jeff ndungu <ndungujeff.ke@gmail.com>
2023-03-16 21:45:19 +0300

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validates :title_does_not_contain
+
+
+    def title_does_not_contain
+        unless title.present? && title.match?(/Won't Believe|Secret|Top [number]|Guess/)
+          errors.add(:title, "something is missing!")
+        end
+      end
+
 end
